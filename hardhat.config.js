0beed4fc2b5b92b21ca5a5b5b4796669f06d3780
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-01-14 15:36:45 +0100

    2022 Jan 14 1

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     rinkeby: {
       url: "https://eth-rinkeby.alchemyapi.io/v2/XU5G5MrpGw2ToNpwRMaIlCnSHV_PL7fD",
-      accounts: ["private key"],
+      accounts: ["ef3213abb147308db9410d549bbc94b03f1901b17645eb75eb0814fd2b9113f6"],
     },
   },
 };
